48d294da374df34da1e3716f20ae50bdf08a7a4d
riojack <riojack@users.noreply.github.com>
2022-03-23 22:12:54 -0500

    pull in pug, get rid of inline plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,20 @@
-const { resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/simulation.js',
-  output: {
-    path: resolve(__dirname, 'dist'),
-    filename: 'app.js',
-  },
   plugins: [
     new HtmlWebpackPlugin({
+      inject: false,
       title: 'Langton\'s Ant',
-      template: './src/template.html',
-      inlineSource: '.(js|css)$',
-      minify: { collapseWhitespace: true },
-    }),
-    new HtmlWebpackInlineSourcePlugin()
+      template: './src/template.pug',
+      minify: { collapseWhitespace: true, removeComments: true, minifyJS: true },
+    })
   ],
   module: {
-    rules: [{
-      test: /\.(js)$/,
-      use: {
-        loader: 'babel-loader',
-      },
-    }]
+    rules: [
+      { test: /\.(js)$/, use: { loader: 'babel-loader' } },
+      { test: /\.pug$/, use: { loader: 'pug-loader' } }
+    ]
   }
 };
